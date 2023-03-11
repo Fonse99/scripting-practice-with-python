745b4f629d1b7c9b3d836a8116303efab8a4a04d
Brandon Isaac Fonseca <isaac99.bf@gmail.com>
2023-03-11 13:27:50 -0600

    Fixing

--- app/Jorge.py
+++ app/Jorge.py
@@ -12,12 +12,11 @@ def data_access():
 
     print('reading from data_access')
     workbook = openpyxl.load_workbook('../fichas-Macro.xlsm')
+    
+    y=10
 
     # Selecciona la hoja de trabajo que contiene los datos
     sheet = workbook['DATOS-indice 2018']
-
-    # establecemos el valor de y en 10 para establecer la distancia de la primera ficha
-    y = 3;
 
     # Itera sobre cada fila y la imprime en la consola
     for index, row in enumerate(sheet.iter_rows(min_row=3, values_only=True)):
@@ -27,7 +26,7 @@ def data_access():
         
         if(index % 3 == 0):
             pdf.add_page()
-            y=3; #reiniciamos el valor al por defecto en cada nueva página
+            y=5; #reiniciamos el valor al por defecto en cada nueva página
 
         # pintar valores en el documento
         ticket_builder(
@@ -84,14 +83,17 @@ def ticket_builder(num=0, names='', last_names='', born_date='', born_place='', 
 
     # region first block
 
-    pdf.rect(x=5, y=y, w=200, h=108)
+    pdf.rect(x=5, y=y, w=200, h=110)
 
     pdf.cell(w=10, h=row_height, txt='No.', align='L',)
     pdf.cell(w=55, h=row_height, txt=str(num), align='C', border='B')
+
     # 2
+
+    pdf.cell(w=10, h=row_height, txt='', align='L', border='0')
     pdf.cell(w=30, h=row_height, txt='En la parroquia de:', align='L', border='0')
     pdf.cell(w=10, h=row_height, txt='', align='R', border='', )
-    pdf.multi_cell(w=85, h=row_height, txt='San Isidro Labrador', align='C', border='B')
+    pdf.multi_cell(w=75, h=row_height, txt='San Isidro Labrador', align='C', border='B')
 
     pdf.cell(w=25, h=row_height, txt='1° Apellido', align='L',)
     pdf.cell(w=40, h=row_height, txt=first_last_name, align='C', border='B')
@@ -149,10 +151,10 @@ def ticket_builder(num=0, names='', last_names='', born_date='', born_place='', 
     pdf.cell(w=20, h=row_height, txt='', align='L', border='B')
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='L', border='0')
-    pdf.cell(w=20, h=row_height, txt='Hijo: ', align='L', border='0')
-    pdf.cell(w=30, h=row_height, txt=hijo, align='C', border='B')
-    pdf.cell(w=30, h=row_height, txt='de', align='C', border='0')
-    pdf.multi_cell(w=35, h=row_height, txt=mother, align='C', border='B')
+    pdf.cell(w=10, h=row_height, txt='Hij: ', align='L', border='0')
+    pdf.cell(w=5, h=row_height, txt="", align='C', border='B')
+    pdf.cell(w=10, h=row_height, txt='de', align='C', border='0')
+    pdf.multi_cell(w=85, h=row_height, txt=mother, align='C', border='B')
 
     pdf.cell(w=10, h=row_height, txt='del', align='L', border='0')
     pdf.cell(w=55, h=row_height, txt='', align='L', border='B')
@@ -193,11 +195,11 @@ def ticket_builder(num=0, names='', last_names='', born_date='', born_place='', 
     pdf.multi_cell(w=55, h=row_height, txt='', align='L', border='B')
     pdf.cell(w=10, h=row_height, txt='Nota: ', align='L', border='0')
     pdf.multi_cell(w=55, h=row_height, txt='', align='L', border='B')
-    pdf.multi_cell(w=55, h=row_jump, txt='', align='L', border='0')
+    pdf.multi_cell(w=55, h=row_jump*2, txt='', align='L', border='0')
 
     # endregion
 
-pdf = FPDF(orientation='P', unit='mm', format='legal')
+pdf = FPDF(orientation='P', unit='mm', format=(216, 390))
 
 pdf.set_font('Arial', '', 10)
 
