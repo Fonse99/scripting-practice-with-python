92847cf9c697b890757990501994c86dffe54c19
Jorge Orozco <creneorozco@gmail.com>
2023-03-15 14:30:00 -0600

    Jorge

--- app/Jorge.py
+++ app/Jorge.py
@@ -3,20 +3,25 @@ import pandas as pd
 import openpyxl
 
 
+
 def data_cleaner(arg: any):
     if arg is None:
         return 'x'
     return arg
 
+
+
 def data_access():
 
-    print('reading from data_access')
+    print('Leyendo datos, por favor espere...')
     workbook = openpyxl.load_workbook('../fichas-Macro.xlsm')
     
     y=10
 
     # Selecciona la hoja de trabajo que contiene los datos
     sheet = workbook['DATOS-indice 2018']
+
+
 
     # Itera sobre cada fila y la imprime en la consola
     for index, row in enumerate(sheet.iter_rows(min_row=3, values_only=True)):
@@ -53,7 +58,7 @@ def data_access():
     workbook.close()
 
 def ticket_builder(num=0, names='', last_names='', born_date='', born_place='', baptism_date='', baptism_place='', godfather='', godmother='s', mother='', father='', y=10, minister='', hijo='Primero'):
-    print('building...')
+    print('Contruyendo...')
 
     row_height = 4.8
     row_jump = 10
@@ -86,48 +91,71 @@ def ticket_builder(num=0, names='', last_names='', born_date='', born_place='', 
     pdf.rect(x=5, y=y, w=200, h=110)
 
     pdf.cell(w=10, h=row_height, txt='No.', align='L',)
+    pdf.set_font("Arial", size=11, style="I")
     pdf.cell(w=55, h=row_height, txt=str(num), align='C', border='B')
+    pdf.set_font('Times', '', 10)
 
     # 2
 
     pdf.cell(w=10, h=row_height, txt='', align='L', border='0')
+    
     pdf.cell(w=30, h=row_height, txt='En la parroquia de:', align='L', border='0')
-    pdf.cell(w=10, h=row_height, txt='', align='R', border='', )
+    
+    pdf.cell(w=10, h=row_height, txt='', align='R', border='', )  
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=75, h=row_height, txt='San Isidro Labrador', align='C', border='B')
+    pdf.set_font('Times', '', 10)
 
     pdf.cell(w=25, h=row_height, txt='1° Apellido', align='L',)
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.cell(w=40, h=row_height, txt=first_last_name, align='C', border='B')
+    pdf.set_font('Times', '', 10)
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='C ', border='', )
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.cell(w=50, h=row_height, txt=baptism_place, align='C', border='B', )
+    pdf.set_font('Times', '', 10)
     pdf.cell(w=10, h=row_height, txt='de', align='C', border='0')
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=55, h=row_height, txt='Diócesis de Bluefields',
                    align='C', border='B')
+    pdf.set_font('Times', '', 10)
 
     pdf.cell(w=25, h=row_height, txt='2° Apellido', align='L',)
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.cell(w=40, h=row_height, txt=second_last_name, align='C', border='B')
+    pdf.set_font('Times', '', 10)
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='C', border='', )
     pdf.cell(w=30, h=row_height, txt='En la fecha de:', align='L',)
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=85, h=row_height, txt=baptism_date, align='C', border='B')
+    pdf.set_font('Times', '', 10)
 
     pdf.cell(w=25, h=row_height, txt='1° Nombre', align='L',)
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.cell(w=40, h=row_height, txt=first_name, align='C', border='B')
+    pdf.set_font('Times', '', 10)
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='C', border='', )
     pdf.cell(w=30, h=row_height, txt='El Ministro: ', align='L',)
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=85, h=row_height, txt=minister, align='C', border='B')
+    pdf.set_font('Times', '', 10)
 
     pdf.cell(w=25, h=row_height, txt='2° Nombre', align='L',)
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=40, h=row_height, txt=second_name, align='C', border='B')
+    pdf.set_font('Times', '', 10)
     pdf.cell(w=65, h=row_jump, txt='', align='C', border='0')
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='C', border='')
     pdf.multi_cell(
         w=130, h=row_height, txt='Administró el Sacramento del Bautismo a:', align='C',)
     pdf.cell(w=75, h=row_jump, txt='', align='C', border='0')
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=115, h=row_height, txt=complete_name_str, align='C', border='B')
-
+    pdf.set_font('Times', '', 10)
     # endregion
 
     # region second block
@@ -136,14 +164,18 @@ def ticket_builder(num=0, names='', last_names='', born_date='', born_place='', 
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='L', border='0')
     pdf.cell(w=30, h=row_height, txt='Quién nació en: ', align='L', border='0')
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=85, h=row_height, txt=born_place, align='C', border='B')
+    pdf.set_font('Times', '', 10)
 
     pdf.cell(w=30, h=row_height, txt='parroquia de', align='L', border='0')
     pdf.cell(w=35, h=row_height, txt=' ', align='C', border='B')
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='L', border='0')
     pdf.cell(w=30, h=row_height, txt='en la fecha de: ', align='L', border='0')
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=85, h=row_height, txt=born_date, align='C', border='B')
+    pdf.set_font('Times', '', 10)
 
     pdf.cell(w=10, h=row_height, txt='el', align='L', border='0')
     pdf.cell(w=25, h=row_height, txt='', align='L', border='B')
@@ -154,14 +186,18 @@ def ticket_builder(num=0, names='', last_names='', born_date='', born_place='', 
     pdf.cell(w=10, h=row_height, txt='Hij: ', align='L', border='0')
     pdf.cell(w=5, h=row_height, txt="", align='C', border='B')
     pdf.cell(w=10, h=row_height, txt='de', align='C', border='0')
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=85, h=row_height, txt=mother, align='C', border='B')
+    pdf.set_font('Times', '', 10)
 
     pdf.cell(w=10, h=row_height, txt='del', align='L', border='0')
     pdf.cell(w=55, h=row_height, txt='', align='L', border='B')
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='L', border='0')
     pdf.cell(w=10, h=row_height, txt='Y de: ', align='L', border='0')
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=105, h=row_height, txt=father, align='L', border='B')
+    pdf.set_font('Times', '', 10)
     pdf.multi_cell(w=20, h=row_jump, txt='', align='L', border='0')
 
     # endregion
@@ -172,21 +208,25 @@ def ticket_builder(num=0, names='', last_names='', born_date='', born_place='', 
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='L', border='0')
     pdf.cell(w=20, h=row_height, txt='Padrinos', align='L', border='0')
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=95, h=row_height, txt=godfather, align='C', border='B')
+    pdf.set_font('Times', '', 10)
 
     pdf.cell(w=30, h=5, txt='parroquia de', align='L', border='0')
     pdf.cell(w=35, h=row_height, txt=' ', align='C', border='B')
     # 2
     pdf.cell(w=10, h=row_height, txt='', align='L', border='0')
     pdf.cell(w=10, h=row_height, txt='Y', align='L', border='0')
+    pdf.set_font("Arial", size=11, style="I") 
     pdf.multi_cell(w=105, h=row_height, txt=godmother, align='C', border='B')
+    pdf.set_font('Times', '', 12)
 
     pdf.cell(w=15, h=row_height, txt='con', align='L', border='0')
     pdf.cell(w=50, h=row_height, txt='', align='L', border='B')
     # 2
     pdf.multi_cell(
         w=130, h=row_height, txt='A quienes se les advirtió su obligación y parentesco espiritual', border='0', align='C')
-
+    pdf.set_font('Times', '', 10)
     pdf.cell(w=10, h=row_height, txt='el', align='L', border='0')
     pdf.cell(w=25, h=row_height, txt='', align='L', border='B')
     pdf.cell(w=10, h=row_height, txt='de', align='L', border='0')
@@ -199,10 +239,19 @@ def ticket_builder(num=0, names='', last_names='', born_date='', born_place='', 
 
     # endregion
 
-pdf = FPDF(orientation='P', unit='mm', format=(216, 390))
+pdf = FPDF(orientation='P', unit='mm', format=(216, 360))
 
-pdf.set_font('Arial', '', 10)
+pdf.set_font('Times', '', 10)
 
 data_access()
+
+class PDF(FPDF):
+    def footer(self):
+        # Go to 1.5 cm from bottom
+        self.set_y(-15)
+        # Select Arial italic 8
+        self.set_font('Arial', 'I', 8)
+        # Print centered page number
+        self.cell(0, 10, 'Page %s' % self.page_no(), 0, 0, 'C')
 
 pdf.output('./report/fichas.pdf')
